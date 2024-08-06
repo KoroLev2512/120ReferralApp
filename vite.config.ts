3ad581e3a97713ac47509d7c764ca2ae7c1d4b13
korolev_2512 <korolev-2512@mail.ru>
2024-08-06 14:50:34 +0500

    fixed problem with @/ path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/referal/",
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
